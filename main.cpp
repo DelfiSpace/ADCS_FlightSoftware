b90c2af9552678a1f9862035f41da6d97098bd3b
Casper <broekhuizen.casper@gmail.com>
2020-04-02 11:39:51 +0200

    Added HWMonitor and ADCManager

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ TMP100 temp(I2Cinternal, 0x48);
 DSPI spi(3);
 MB85RS fram(spi, GPIO_PORT_P1, GPIO_PIN0 );
 
+// HardwareMonitor
+HWMonitor hwMonitor(&fram);
+
 // Bootloader
 Bootloader bootLoader = Bootloader(fram);
 
@@ -77,6 +80,7 @@ void acquireTelemetry(ADCSTelemetryContainer *tc)
 
     // set uptime in telemetry
     tc->setUpTime(uptime);
+    //tc->setMCUTemperature(hwMonitor.getMCUTemp());
 
     // measure the power bus
     tc->setBusStatus((!powerBus.getVoltage(v)) & (!powerBus.getCurrent(i)));
@@ -113,6 +117,11 @@ void main(void)
     // - clock tree
     DelfiPQcore::initMCU();
 
+    // initialize the ADC
+    // - ADC14 and FPU Module
+    // - MEM0 for internal temperature measurements
+    ADCManager::initADC();
+
     // Initialize I2C master
     I2Cinternal.setFastMode();
     I2Cinternal.begin();
@@ -143,6 +152,12 @@ void main(void)
     // - initialize the pins for the hardware watch-dog
     // - prepare the pin for power cycling the system
     reset.init();
+
+
+    // initialize HWMonitor readings
+    hwMonitor.readResetStatus();
+    hwMonitor.readCSStatus();
+
 
     // link the command handler to the PQ9 bus:
     // every time a new command is received, it will be forwarded to the command handler
